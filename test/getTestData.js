bd1396d47395eec2848de76ae797db557e8578dc
Ville Rantala <ville.rantala@microsoft.com>
2017-10-05 15:30:36 +0200

    Add script to generate test date

---
+++ test/getTestData.js
@@ -0,0 +1,21 @@
+var temperature = Math.random() * (25 - 20) + 20;
+var data = temperature.toFixed(2) + ':36.21';
+
+var testData = {
+  'cmd': 'rx',
+  'seqno': 1854,
+  'EUI': 'BE7A00000000190F',
+  'ts': 1507044971381,
+  'fcnt': 27,
+  'port': 1,
+  'freq': 867100000,
+  'rssi': -25,
+  'snr': 10,
+  'toa': 61,
+  'dr': 'SF7',
+  'ack': false,
+  'bat': 255,
+  'data': Buffer.from(data).toString('hex')
+};
+
+console.log(JSON.stringify(testData));
